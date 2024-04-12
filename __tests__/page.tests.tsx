1151539e4d5ad6d5d8fe8cb6c146b09278db083f
Srijan <srijan.vaddadi@gmail.com>
2024-04-12 12:34:12 +0100

    testing

--- __tests__/page.tests.tsx
+++ __tests__/page.tests.tsx
@@ -1,14 +1,42 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import Page from '../src/app/page'
+import Apicall from "@/app/apicall";
+import axios from "axios";
 
-describe('Page', ()=> {
-    it('renders a heading',  () => {
-        render(<Page />)
+jest.mock('axios');
 
-        const heading = screen.getByRole('heading', { level: 1 })
+describe('Apicall function', () => {
+  it('fetches player data from the API', async () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    const mockPlayerData = {
+      firstname: 'John',
+      lastname: 'Doe',
+      age: 25,
+      photo: 'https://example.com/photo.jpg',
+      id: 1,
+      season: 2023,
+    };
 
-        expect(heading).toBeInTheDocument()
-        expect(heading).toHaveTextContent('Welcome to Next.js')
-    })
-})
+    const mockedApiResponse = {
+      data: {
+        response: [ { player: mockPlayerData } ]
+      }
+    };
+
+    mockedAxios.request.mockResolvedValue(mockedApiResponse);
+
+    const playerId = '1';
+    const season = '2023';
+
+    const player = await Apicall(playerId, season);
+
+    expect(player).toEqual(mockPlayerData);
+    expect(mockedAxios.request).toHaveBeenCalledWith({
+      method: 'GET',
+      url: 'https://api-football-v1.p.rapidapi.com/v3/players',
+      headers: {
+        'X-RapidAPI-Key': 'bab6dbce6dmsh39602424deb690ap1f6557jsn0e26ef03544d',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+      },
+      params: { id: playerId, season },
+    });
+  });
+});
